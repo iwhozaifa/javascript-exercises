d6b021b0b517189f170bb1d1875e9d04d2da2e20
iwhozaifa <alihuzaifa7204@gmail.com>
2026-02-10 02:39:06 +0500

    Foundations 06
    Completed the repeatString problem

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  // num = parseInt(prompt("Enter a number"));
+  // string = (prompt("Enter a string"))
+  
+  let result = ""
 
+  if (num < 0) {
+    return "ERROR"
+  }
+  for(let i = 0; i < num ; i++){
+  result += string;
+  }
+  return result
 };
 
 // Do not edit below this line
